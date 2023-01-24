c133a23f0f116f4f68179a55eb5ed2d7d49ec5e6
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-24 23:06:46 +0530

    [Prashant]Add: Repeat till the Player reaches the winning position

--- SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -11,37 +11,51 @@ namespace SnakeLadderProgram
         public const int LADDER = 1, SNAKE = 2;
         public void Game()
         {
-            int position1 = 1;
+
+            int position1 = 0;
             Console.WriteLine("The Position of the Player is: " + position1);
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            Console.WriteLine("The number got by dice roll is: " + dice);
-            Random random1= new Random();
-            int option = random1.Next(0,3);
-            switch(option)
+            do
             {
-                case LADDER:
-                    position1 += dice;
-                    Console.WriteLine("The Player climb the Ladder");
-                    break;
-                case SNAKE:
-                    position1 -= dice;
-                    if(position1 < 0)
-                    {
-                        Console.WriteLine("The Player bit by Snake");
-                        position1= 0;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The Player bit by Snake");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("No Play");
-                    break;
-            }
-            Console.WriteLine("The Playr position is: " + position1);
-            Console.ReadLine();
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("The number got by dice roll is: " + dice);
+                Random random1 = new Random();
+                int option = random1.Next(0, 3);
+                switch (option)
+                {
+                    case LADDER:
+                        position1 += dice;
+                        Console.WriteLine("The Player climb the Ladder");
+                        break;
+                    case SNAKE:
+                        position1 -= dice;
+                        if (position1 < 0)
+                        {
+                            Console.WriteLine("The Player bit by Snake");
+                            position1 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The Player bit by Snake");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;
+                }
+                Console.WriteLine("The Playr position is: " + position1);
+                
+
+            } while (position1 < 100);
         }
     }
 }
+
+
+
+            
+    
+
+        
+        
+    
