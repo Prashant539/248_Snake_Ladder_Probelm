b05aede33ae0dc0190dfeba9e9f5ea9c60af2f82
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-24 21:59:19 +0530

    [Prashant]Add: Game start with position 0

---
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SnakeLadderProgram
+{
+    internal class Progrma
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake ladder Program ");
+            Console.WriteLine("Enter the Number to Perform the Program: ");
+            Console.WriteLine("1:Position");
+            int Options = Convert.ToInt32(Console.ReadLine());
+            switch(Options)
+            {
+                case 1:
+                    SnakeLadder snakeLadder = new SnakeLadder();
+                    snakeLadder.Game();
+                    break;
+            }
+        }
+    }
+}
---
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProgram
+{
+    public class SnakeLadder
+    {
+        public void Game()
+        {
+            int position = 0;
+            Console.WriteLine("The Position of the Player is: " + position);
+        }
+    }
+}
