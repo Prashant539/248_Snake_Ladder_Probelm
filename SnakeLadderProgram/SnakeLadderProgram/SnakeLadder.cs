353f382e5ec9640712b08e1582a7dac6237220f7
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-24 22:26:59 +0530

    [Prashant]Add: The Player then checks for a Option

--- SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -8,13 +8,40 @@ namespace SnakeLadderProgram
 {
     public class SnakeLadder
     {
+        public const int LADDER = 1, SNAKE = 2;
         public void Game()
         {
-            int position = 0;
-            Console.WriteLine("The Position of the Player is: " + position);
+            int position1 = 1;
+            Console.WriteLine("The Position of the Player is: " + position1);
             Random random = new Random();
             int dice = random.Next(1, 7);
             Console.WriteLine("The number got by dice roll is: " + dice);
+            Random random1= new Random();
+            int option = random1.Next(0,3);
+            switch(option)
+            {
+                case LADDER:
+                    position1 += dice;
+                    Console.WriteLine("The Player climb the Ladder");
+                    break;
+                case SNAKE:
+                    position1 -= dice;
+                    if(position1 < 0)
+                    {
+                        Console.WriteLine("The Player bit by Snake");
+                        position1= 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Player bit by Snake");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
+            Console.WriteLine("The Playr position is: " + position1);
+            Console.ReadLine();
         }
     }
 }
